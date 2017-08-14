4e917839623703c5d559846dfc01f89413cef565
Nick Hoats <nhoatz@gmail.com>
2017-08-14 16:16:28 -0400

    finished the boot state

--- js/boot.js
+++ js/boot.js
@@ -3,12 +3,15 @@ starPower.boot = function () {};
 
 starPower.boot.prototype = {
   preload: function () {
+    game.load.image('logo', '../assets/logo.png');
 
   },
 
   create: function () {
+    this.game.stage.backgroundColor = '#212121';
 
-    
+    this.game.physics.startSystem(phaser.Physics.Arcade);
+
     this.state.start('preloader');
   }
 };
